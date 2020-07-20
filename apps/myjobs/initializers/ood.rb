4feb28b0b65ade8da517502dd15812093e151785
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-07-19 20:18:03 -0400

    updates

---
+++ apps/myjobs/initializers/ood.rb
@@ -0,0 +1,8 @@
+#module OodAppkit
+#  module Configuration
+#    def dataroot
+#      root = "/home/#{ENV['USER']}/ondemand/data/sys/other-myjobs-location/"
+#      Pathname.new(root).expand_path
+#    end
+#  end
+#end
--- profile
+++ profile
@@ -1,2 +1,6 @@
-HOME="/users/$USER"
-FOO="bar"
+#HOME="/users/$USER"
+#FOO="bar"
+
+#LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu"
+#GEM_HOME=/usr/local/share/gems
+#GEM_PATH=/usr/local/share/gems:/usr/share/gems
